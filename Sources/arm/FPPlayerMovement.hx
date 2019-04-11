b21886fe60c11b2452e417a73e4d27449fd07955
sheltongabe <sheltongabe0@gmail.com>
2019-04-11 16:05:51 -0400

    Setup camera mostly and begin aim code

--- Sources/arm/FPPlayerMovement.hx
+++ Sources/arm/FPPlayerMovement.hx
@@ -1,13 +1,27 @@
 package arm;
 
+import iron.math.Vec4;
+import iron.system.Input;
 import armory.trait.physics.bullet.RigidBody;
 import iron.object.Object;
 import iron.object.CameraObject;
 import armory.trait.physics.PhysicsWorld;
 
+enum Action {
+	FORWARD;
+	BACKWARD;
+	STRAFE_LEFT;
+	STRAFE_RIGHT;
+}
+
 class FPPlayerMovement extends iron.Trait {
+	/// Force to apply
+	var appliedForce:Float;
+
 	/// Camera
 	var camera:Object;
+
+	/// Rigid body for the player object
 	var body:RigidBody;
 
 	/// Constructor
@@ -37,6 +51,62 @@ class FPPlayerMovement extends iron.Trait {
 		// Make sure the body is ready
 		if(!body.ready)
 			return;
+		
+		// Update properties
+		this.appliedForce = object.properties["APPLY_FORCE"];
+
+		// Apply Movement Forces
+		var actions:Array<Action> = this.getKeyActions();
+		for(action in actions) {
+			switch(action) {
+				case FORWARD:
+					body.applyForce(new Vec4(0.0, this.appliedForce));
+					break;
+
+				case BACKWARD:
+					body.applyForce(new Vec4(0.0, -this.appliedForce));
+					break;
+
+				case STRAFE_LEFT:
+					body.applyForce(new Vec4(-this.appliedForce));
+					break;
+
+				case STRAFE_RIGHT:
+					body.applyForce(new Vec4(this.appliedForce));
+					break;
+			}
+		}
+
+		// Sync the transformations made between the physics and transformation matrix
+		body.syncTransform();
+	}
+
+	/// Change Direction with mouse input
+	private function Aim() {
+		var mouse:iron.system.Input.Mouse = Input.getMouse();
+		var dx = mouse.movementX;
+		var dy = mouse.movementY;
+
+	}
+
+	/**
+	 * @brief	Get any key presses and return them as Key Actions
+	 * @return	An array of actions that are to be done
+	 */
+	private function getKeyActions():Array<Action> {
+		var actions:Array<Action> = new Array<Action>();
+		var keyboard:Keyboard = Input.getKeyboard();
+
+		if(keyboard.down("w"))
+			actions.push(FORWARD);
+		if(keyboard.down("s"))
+			actions.push(BACKWARD);
+		if(keyboard.down("a"))
+			actions.push(STRAFE_LEFT);
+		if(keyboard.down("d"))
+			actions.push(STRAFE_RIGHT);
+
+		return actions;
 	}
 
 	/**
@@ -46,3 +116,4 @@ class FPPlayerMovement extends iron.Trait {
 
 	}
 }
+
