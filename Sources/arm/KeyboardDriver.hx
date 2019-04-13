f7a4baf0fa63f929c73d80c0a3e167ba891631ed
sheltongabe <sheltongabe0@gmail.com>
2019-04-13 16:59:12 -0400

    Add a keyboard Driver to handle all key testing

---
+++ Sources/arm/KeyboardDriver.hx
@@ -0,0 +1,55 @@
+package arm;
+
+/**
+ * @file	KeyboardDriver.hx
+ * @brief	Handle all keyboard input and provide an index
+ */
+
+import iron.system.Input;
+import kha.input.KeyCode;
+import iron.system.Input.Keyboard;
+
+/**
+ * @class 	KeyboardDriver
+ * @brief	Provide an index for if keys are pressed
+ */
+class KeyboardDriver {
+	/// String for Escape
+	public static var ESCAPE:String = Keyboard.keyCode(KeyCode.Escape);
+
+	/// Keyboard to use
+	var keyboard:Keyboard;
+
+	/// Map of keyboard controls to a boolean of them being pressed
+	var keyIndex:Map<String, Bool>;
+
+	/// Constructor
+	public function new() {
+		this.keyboard = Input.getKeyboard();
+
+		// Key index
+		this.keyIndex = [
+			"w" => false,
+			"a" => false,
+			"s" => false,
+			"d" => false,
+			ESCAPE => false
+		];
+	}
+
+	/// Update the keyIndex with the keyboard
+	public function update() {
+		for(key in this.keyIndex.keys()) {
+			this.keyIndex[key] = this.keyboard.down(key);
+		}
+	}
+
+	/**
+	 * @brief	Return if the passed key was pressed
+	 * @param	key		Key to test
+	 * @return	Is the key pressed
+	 */
+	public function isPressed(key:String):Bool {
+		return this.keyIndex[key];
+	}
+}
